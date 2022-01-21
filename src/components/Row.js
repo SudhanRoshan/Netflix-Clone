763f0836e83c08efb87c06f45efe555b97a78a4e
SudhanRoshan <19tucs233@skct.edu.in>
2022-01-21 14:58:35 +0530

    fix(ApiCall):api call changed

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,74 +1,76 @@
-import React from 'react';
+import React from "react";
 import "../styles/Row.css";
 import axios from "../axios";
-import {useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import YouTube from "react-youtube";
-import movieTrailer from 'movie-trailer';
+import movieTrailer from "movie-trailer";
 
-
-
-function Row({title,fetchUrl,isLargeRow=false}) {
-    const base_url="https://image.tmdb.org/t/p/original/";
-    const [movies,setMovies]=useState([]);
-    const [trailerUrl,setTrailerUrl]=useState("")
-    useEffect(()=>{
-        async function fetchData(){
-            const request=await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData()
-},[fetchUrl]);
-
-const opts={
-    height:"390",
-    width:"100%",
-    playerVars:{
-        autoplay:1,
-    },
-};
-
-const handleClick=(movie)=>{
-    if (trailerUrl){
-        setTrailerUrl("");
-    }else{
-        movieTrailer(movie?.name || "")
-        .then((url)=>{
-            const urlParams=new URLSearchParams(new URL(url).search);
-            setTrailerUrl(urlParams.get("v"));
-        })
-        .catch((error)=>console.log(error))
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const base_url = "https://image.tmdb.org/t/p/original/";
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
     }
+    fetchData();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = async(movie) => {
+      if (trailerUrl) {
+        setTrailerUrl("");
+    } else {
+        const url = await movieTrailer(movie?.title || "")
+        console.log("url")
+        console.log(url)
+        console.log("url")
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get("v"))
+    }
+  };
+
+  return (
+    <div className="row">
+      <h3>{title}</h3>
+      <div className="row_posters">
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                key={movie.id}
+                onClick={() => handleClick(movie)}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              ></img>
+            )
+        )}
+      </div>
+      {trailerUrl && (
+        <YouTube
+          videoId={trailerUrl}
+          opts={opts}
+          onReady={(event) => {
+            console.log(event);
+            event.target.pauseVideo();
+          }}
+       />
+      )}
+    </div>
+  );
 }
 
-
-
-    return (
-        <div className="row">
-            <h3>{title}</h3>
-            <div className="row_posters">
-            {movies.map(
-                (movie)=>
-                ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && 
-                (
-                <img
-               className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-               key={movie.id}
-               onClick={()=>handleClick(movie)}
-               src={`${base_url}${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-               }`}
-                alt={movie.name}>
-
-                </img>
-                
-                )
-           )}
-            </div>
-            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}></YouTube>}
-          
-        </div>
-    )
-} 
-
-export default Row
+export default Row;
